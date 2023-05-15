0a27f0353fa62093b3dbabaf32f9ba1ca1662436
Vinayak-0202 <vpnikam02@gmail.com>
2023-05-16 02:11:47 +0530

    Implement the controllersr by using monogoDB dataBase

--- controllers/tourControllerRoute.js
+++ controllers/tourControllerRoute.js
@@ -1,50 +1,60 @@
 const express = require('express');
 const Tour = require('../models/tours.js');
 
-//This param middleware call back function check the data is in correct format or not
-exports.checkBody = (req, res, next) => {
-  // console.log(req.body);
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
+exports.getAllTours = async (req, res) => {
+  console.log(req.requestTime);
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
       status: 'fail',
-      message: 'Bad Request Responce',
+      message: 'Invalid Request',
     });
   }
-
-  next();
 };
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestTime: req.requestTime,
-    // data: {
-    //   toursData,
-    // },
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    //Tour.findOne({_id:req.param.id})
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1; //when we multiply string of digits by 1 it converts the string to number
-
-  //if(id > toursData.length)
-  res.status(200).json({
-    status: 'success',
-    // data: {
-    //   tour,
-    // },
-  });
-};
-
-exports.addTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.addTour = async (req, res) => {
+  // const newTour= new Tour({});
+  // newTour.save();
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
