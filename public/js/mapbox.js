7cf097238e2f7e5666c70d05ea6ceb9eb9427942
Vinayak-0202 <vpnikam02@gmail.com>
2023-06-28 23:33:06 +0530

    working on map

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,2 +1,10 @@
 const locations = document.getElementById('map').dataset.locations;
 console.log(locations);
+L.mapquest.key = '0zWOmSlU3x9j4BKOKCcKWiAcMMTde7gT';
+
+// 'map' refers to a <div> element with the ID map
+L.mapquest.map('map', {
+  center: [37.7749, -122.4194],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12,
+});
