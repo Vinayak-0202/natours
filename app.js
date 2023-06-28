cb4b4b13c4d640474a6da3e5c23d36fbe28b64ed
Vinayak-0202 <vpnikam02@gmail.com>
2023-06-29 01:21:00 +0530

    working on maps

--- app.js
+++ app.js
@@ -92,6 +92,30 @@ app.all('*', (req, res, next) => {
   next(new AppError(404, 'Cant determine the route that you enterd'));
 });
 
+const scriptSrcUrls = ['https://unpkg.com/', 'https://tile.openstreetmap.org'];
+const styleSrcUrls = [
+  'https://unpkg.com/',
+  'https://tile.openstreetmap.org',
+  'https://fonts.googleapis.com/',
+];
+const connectSrcUrls = ['https://unpkg.com', 'https://tile.openstreetmap.org'];
+const fontSrcUrls = ['fonts.googleapis.com', 'fonts.gstatic.com'];
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'blob:'],
+      objectSrc: [],
+      imgSrc: ["'self'", 'blob:', 'data:', 'https:'],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
+);
+
 //Global Error Handling middleware
 app.use(globalErrorHandler);
 //START SERVER
